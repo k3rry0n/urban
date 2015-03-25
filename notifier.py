2e5135f5cb17607c96288692e3aca21a690e9032
Alvus <alvus85@gmail.com>
2015-03-25 19:00:52 +0200

    notify

---
+++ notifier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import os, subprocess, urban
+# cunt
+n = subprocess.Popen("xsel -o", shell=True, stdout=subprocess.PIPE)
+
+string, status = n.communicate()
+a = urban.Urban(string)
+meanings = a.get_meanings() 
+meanings, examples = meanings
+result = "{}\n============\nEXAMPLE: {}".format(meanings[0], examples[0])
+os.system("notify-send '<b>{}</b>' '{}'".format(string.decode(), result))
